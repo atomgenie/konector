d16373e1de4383d91c530d97ec28753a35d119fc
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2021-02-10 19:56:56 +0100

    fix: Fix bash

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ echo "You need to define the GITHUB_USER env"
 exit 1
 fi
 
-curl -sfL -o konector https://github.com/atomgenie/konector/releases/download/1.0.0/konector &&
-chmod u+x konector &&
-sudo mv konector /usr/bin &&
-konector init --username=$GITHUB_USER &&
-sudo konector init-systemctl --user=$user &&
+curl -sfL -o konector https://github.com/atomgenie/konector/releases/download/1.0.0/konector
+chmod u+x konector
+sudo mv konector /usr/bin
+konector init --username=$GITHUB_USER
+sudo konector init-systemctl --user=$USER
 systemctl enable konector
